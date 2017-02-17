434797a033c582db2ee2dc52901874d806a70985
Andrew Stephan <thekiteeatingtree@gmail.com>
2017-02-17 17:29:14 -0500

    fix error creating new file from 'no file'

--- app/js/lib/path.js
+++ app/js/lib/path.js
@@ -1,4 +1,8 @@
 module.exports.dirname = function(path) {
+    if (typeof path === 'undefined') {
+        return '';
+    }
+    
     if (path[path.length - 1] === '/') {
         path = path.substring(0, path.length - 1);
     }
