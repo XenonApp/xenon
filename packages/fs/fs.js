f513be7673fd2870b7ccc82dbc48e87255505772
Andrew Stephan <thekiteeatingtree@gmail.com>
2018-02-19 16:45:17 -0500

    start to update xedd

---
+++ packages/fs/fs.js
@@ -0,0 +1,241 @@
+const async = require('async');
+const chokidar = require('chokidar');
+const nodeFs = require("fs");
+const path = require("path");
+
+const spawn = require("child_process").spawn;
+
+module.exports = function(dir) {
+    let rootPath = dir;
+    let watcher = null;
+
+    // Support opening a single file
+    var stats = nodeFs.statSync(rootPath);
+    var filename, newRoot, vcsStat;
+    if (stats.isFile()) {
+        var vcsFound = false;
+        filename = rootPath;
+        do {
+            // Scan up the file tree looking for version control dirs.
+            newRoot = path.dirname(rootPath);
+            if (newRoot == rootPath) {
+                // No VCS found, give up.
+                rootPath = path.dirname(filename);
+                break;
+            } else {
+                rootPath = newRoot;
+            }
+            [".bzr", ".git", ".svn", ".hg", ".fslckout", "_darcs", "CVS"].some(function(vcs) {
+                try {
+                    vcsStat = nodeFs.statSync(path.join(rootPath, vcs));
+                    vcsFound = true;
+                    return true;
+                } catch(ignore) {}
+            });
+        } while (!vcsFound);
+        filename = stripRoot(filename).slice(1);
+    }
+
+    function dirname(path) {
+        if (path[path.length - 1] === '/') {
+            path = path.substring(0, path.length - 1);
+        }
+        var parts = path.split("/");
+        return parts.slice(0, parts.length - 1).join("/");
+    }
+
+    function stripRoot(filename) {
+        return filename.substring(rootPath.length);
+    }
+
+    function addRoot(filename) {
+        return rootPath + filename;
+    }
+
+    function mkdirs(path) {
+        return new Promise(function(resolve, reject) {
+            var parts = path.split("/");
+            if (parts.length === 1) {
+                resolve();
+            } else {
+                mkdirs(parts.slice(0, parts.length - 1).join("/")).then(function() {
+                    nodeFs.exists(path, function(exists) {
+                        if (exists) {
+                            resolve();
+                        } else {
+                            nodeFs.mkdir(path, function(err) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    resolve(err);
+                                }
+                            });
+                        }
+                    });
+                }, reject);
+            }
+        });
+    }
+    
+    const api = {};
+
+    api.listFiles = function() {
+        var files = [];
+
+        return new Promise(function(resolve, reject) {
+            function readDir(dir, callback) {
+                nodeFs.readdir(dir, function(err, entries) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    async.each(entries, function(entry, next) {
+                        // if (entry[0] === ".") {
+                        //     return next();
+                        // }
+                        var fullPath = dir + "/" + entry;
+                        nodeFs.stat(fullPath, function(err, stat) {
+                            if (err) {
+                                return next(err);
+                            }
+                            if (stat.isDirectory()) {
+                                readDir(fullPath, next);
+                            } else {
+                                files.push(fullPath);
+                                next();
+                            }
+                        });
+                    }, callback);
+                });
+            }
+            readDir(rootPath, function(err) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(files.map(stripRoot));
+            });
+        });
+    };
+
+    api.readFile = function(path, binary) {
+        if (path === "/.zedstate" && filename) {
+            return Promise.resolve(JSON.stringify({
+                "session.current": ["/" + filename]
+            }));
+        }
+        var fullPath = addRoot(path);
+        return new Promise(function(resolve, reject) {
+            nodeFs.readFile(fullPath, {
+                encoding: binary ? 'binary' : 'utf8'
+            }, function(err, contents) {
+                if (err) {
+                    return reject(err);
+                }
+                nodeFs.stat(fullPath, function(err, stat) {
+                    if (err) {
+                        console.error("Readfile successful, but error during stat:", err);
+                    }
+                    watcher.setCacheTag(path, "" + stat.mtime);
+                    resolve(contents);
+                });
+            });
+        });
+    };
+    
+    api.writeFile = function(path, content, binary) {
+        if (path === "/.zedstate" && filename) {
+            return Promise.resolve();
+        }
+        var fullPath = addRoot(path);
+        // First ensure parent dir exists
+        return mkdirs(dirname(fullPath)).then(function() {
+            return new Promise(function(resolve, reject) {
+                nodeFs.writeFile(fullPath, content, {
+                    encoding: binary ? 'binary' : 'utf8'
+                }, function(err) {
+                    if (err) {
+                        return reject(err);
+                    }
+                    nodeFs.stat(fullPath, function(err, stat) {
+                        watcher.setCacheTag(path, "" + stat.mtime);
+                        resolve();
+                    });
+                });
+            });
+        });
+    };
+    
+    api.deleteFile = function(path) {
+        var fullPath = addRoot(path);
+        return new Promise(function(resolve, reject) {
+            nodeFs.unlink(fullPath, function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        });
+    };
+    
+    api.watch = function(ignored, callback) {
+        if (watcher !== null) {
+            watcher.close();
+        }
+        
+        watcher = chokidar.watch(rootPath, {
+            ignored,
+            persistent: true
+        });
+        
+        return watcher;
+    }
+    
+    api.getCacheTag = function(path) {
+        return new Promise(function(resolve, reject) {
+            nodeFs.stat(addRoot(path), function(err, stat) {
+                if (err) {
+                    return reject(404);
+                }
+                resolve("" + stat.mtime);
+            });
+        });
+    };
+    
+    api.getProjectPath = function() {
+        return rootPath;
+    };
+    
+    api.getCapabilities = function() {
+        return {
+            run: true
+        };
+    };
+    
+    api.run = function(command, stdin) {
+        return new Promise(function(resolve) {
+            var p = spawn(command[0], command.slice(1), {
+                cwd: rootPath,
+                env: process.env
+            });
+            var chunks = [];
+            if (stdin) {
+                p.stdin.end(stdin);
+            }
+            p.stdout.on("data", function(data) {
+                chunks.push(data);
+            });
+            p.stderr.on("data", function(data) {
+                chunks.push(data);
+            });
+            p.on("close", function() {
+                resolve(chunks.join(''));
+            });
+            p.on("error", function(err) {
+                // Not rejecting to be compatible with webfs implementation
+                resolve(chunks.join('') + err.message);
+            });
+        });
+    };
+
+    return api;
+};
---
+++ packages/xedd/socket.js
@@ -0,0 +1,410 @@
+#!/usr/bin/env node
+
+const app = require('express')();
+const http = require('http');
+const socketIO = require('socket.io');
+const https = require("https");
+const pathlib = require("path");
+const urllib = require("url");
+const fs = require("fs");
+const qs = require("querystring");
+const mkdirp = require("mkdirp");
+const mime = require("mime");
+const auth = require("basic-auth");
+const nconf = require("nconf");
+const spawn = require("child_process").spawn;
+const packageVersion = require('./package.json').version;
+
+let io;
+
+/**
+ * Options:
+ * - user
+ * - pass
+ * - port
+ * - remote
+ * - enable-run
+ * - root
+ * - tls-key
+ * - tls-cert
+ */
+
+var config = nconf.argv().env().file(process.env.HOME + "/.xeddrc").defaults({
+    port: 7337,
+    ip: "0.0.0.0",
+    root: process.env.HOME || "/"
+});
+
+if (!config.get("user") && config.get("ip") === "0.0.0.0") {
+    config.set("ip", "127.0.0.1");
+}
+
+var ROOT = pathlib.resolve(config.get("root"));
+var enableRun = !config.get("remote") || config.get("enable-run");
+
+switch (process.argv[2]) {
+    case "--help":
+        help();
+        break;
+    case "--version":
+        version();
+        break;
+    default:
+        start();
+}
+
+function help() {
+    console.log(`
+Xedd is the Xenon daemon used to edit files either locally or remotely using Xenon.
+Options can be passed in either as environment variables, JSON config in
+~/.xeddrc or as command line arguments prefixed with '--':
+
+   user:       username to use for authentication (default: none)
+   pass:       password to use for authentication (default: none)
+   remote:     bind to 0.0.0.0, requires auth, and disables
+               enable-run by default
+   port:       port to bind to (default: 7337)
+   root:       root directory to expose (default: $HOME)
+   enable-run: enable running of external programs in remote mode
+   tls-key:    path to TLS key file (enables https)
+   tls-cert:   path to TLS certificate file (enables https)
+    `);
+}
+
+function version() {
+    console.log(`Xedd version ${packageVersion}`);
+}
+
+function start() {
+    var server, isSecure;
+    var bindIp = config.get("remote") ? "0.0.0.0" : "127.0.0.1";
+    var bindPort = config.get("port");
+    if (config.get("remote") && !config.get("user")) {
+        console.error("In remote mode, --user and --pass need to be specified.");
+        process.exit(1);
+    }
+    if (config.get("tls-key") && config.get("tls-cert")) {
+        server = https.createServer({
+            key: fs.readFileSync(config.get("tls-key")),
+            cert: fs.readFileSync(config.get("tls-cert"))
+        }, app);
+        isSecure = true;
+    } else {
+        server = http.createServer(app);
+        isSecure = false;
+    }
+    io = socketIO(server);
+    server.listen(bindPort, bindIp);
+    server.on('error', function() {
+        console.error('ERROR: Could not listen on port', bindPort, 'is xedd already running?');
+        process.exit(2);
+    });
+    console.log(`
+Xedd is now listening on ${isSecure ? 'https' : 'http'} ://${bindIp}:${bindPort}
+Exposed filesystem : ${ROOT},
+Mode               : ${config.get('remote') ? 'remote (externally accessible)' : 'local'}
+Command execution  : ${enableRun ? 'enabled' : 'disabled'}
+Authentication     : ${config.get('user') ? 'enabled' : 'disabled'}
+    `);
+}
+
+
+function error(res, code, message) {
+    res.writeHead(code, {
+        "Content-Type": "text/plain"
+    });
+    res.write(code + " " + message);
+    res.end();
+}
+
+function sendEtagHeader(res, stat) {
+    res.setHeader("ETag", stat.mtime.getTime());
+}
+
+function doGet(req, res, filePath) {
+    fs.stat(filePath, function(err, stat) {
+        if (err) {
+            return error(res, 404, "Path not found");
+        }
+
+        res.statusCode = 200;
+        sendEtagHeader(res, stat);
+        if (stat.isDirectory()) {
+            res.setHeader('Content-Type', 'text/plain');
+            fs.readdir(filePath, function(err, list) {
+                list = list.forEach(function(name) {
+                    try {
+                        var stat = fs.statSync(filePath + "/" + name);
+                        if (stat.isDirectory()) {
+                            res.write(name + "/\n");
+                        } else {
+                            res.write(name + "\n");
+                        }
+                    } catch (e) {
+                        // Broken symlink or something, ignore
+                    }
+                });
+                res.end();
+            });
+        } else { // File
+            res.setHeader('Content-Length', stat.size);
+            res.setHeader('Content-Type', mime.lookup(filePath));
+            var stream = fs.createReadStream(filePath);
+            stream.on('error', function(err) {
+                console.error("Error while reading", err);
+            });
+            stream.pipe(res);
+        }
+
+    });
+}
+
+function doHead(req, res, filePath) {
+    fs.stat(filePath, function(err, stat) {
+        if (err) {
+            return error(res, 404, "Path not found");
+        }
+
+        res.statusCode = 200;
+        sendEtagHeader(res, stat);
+        res.setHeader("Content-Length", "0");
+        res.end("");
+    });
+}
+
+function doPut(req, res, filePath) {
+    // TODO: Make this streaming (and in that case: write to temp file first)
+    var chunks = [];
+
+    req.on("data", function(chunk) {
+        chunks.push(chunk);
+    });
+    req.on("error", function() {
+        error(res, 500, "Could't save file");
+    });
+    req.on("end", function() {
+        var parentDir = pathlib.dirname(filePath);
+        mkdirp(parentDir, function(err) {
+            if (err) {
+                return error(res, 500, "Could't create parent directory");
+            }
+            var f = fs.createWriteStream(filePath);
+            f.on('error', function() {
+                error(res, 500, "Couldn't write to file");
+            });
+            // Flush out all chunks
+            for (var i = 0; i < chunks.length; i++) {
+                f.write(chunks[i]);
+            }
+            f.end();
+            f.on('finish', function() {
+                fs.stat(filePath, function(err, stat) {
+                    if (err) {
+                        return error(res, 404, "Path not found");
+                    }
+
+                    res.statusCode = 200;
+                    sendEtagHeader(res, stat);
+                    res.end("OK");
+                });
+            });
+        });
+    });
+}
+
+function doDelete(req, res, filePath) {
+    fs.stat(filePath, function(err) {
+        if (err) {
+            return error(res, 404, "File not found");
+        }
+
+        fs.unlink(filePath, function(err) {
+            if (err) {
+                return error(res, 404, "Unable to remove file");
+            }
+            res.statusCode = 200;
+            res.end("OK");
+        });
+    });
+}
+
+function doPost(req, res, filePath) {
+    bufferPostRequest(req, res, function() {
+        var action = res.post && res.post.action;
+        switch (action) {
+            case "filelist":
+                res.writeHead(200, "OK", {
+                    'Content-Type': 'text/plain'
+                });
+                fileList(filePath, res);
+                break;
+            case "run":
+                if (!enableRun) {
+                    return error(res, 403, "Run is disabled");
+                }
+                runCommand(filePath, res);
+                break;
+            case "version":
+                res.writeHead(200, "OK", {
+                    'Content-Type': 'text/plain'
+                });
+                res.end("1.1");
+                break;
+            case "capabilities":
+                res.writeHead(200, "OK", {
+                    'Content-Type': 'application/json'
+                });
+                res.end(JSON.stringify({
+                    run: !!enableRun
+                }));
+                break;
+            default:
+                return error(res, 404, "Unable to perform action: " + action);
+        }
+    });
+}
+
+function requestHandler(req, res) {
+    var filePath = decodeURIComponent(urllib.parse(req.url).path);
+    if (config.get("user")) {
+        var user = auth(req);
+        if (!user || user.name !== config.get("user") || user.pass !== config.get("pass")) {
+            res.writeHead(401, {
+                'WWW-Authenticate': 'Basic realm="Zed daemon"'
+            });
+            return res.end();
+        }
+    }
+    console.log(req.method, filePath);
+    filePath = pathlib.normalize(pathlib.join(ROOT, filePath));
+
+    if (filePath.indexOf(ROOT) !== 0) {
+        return error(res, 500, "Hack attempt?");
+    }
+
+    switch (req.method) {
+        case "GET":
+            return doGet(req, res, filePath);
+        case "HEAD":
+            return doHead(req, res, filePath);
+        case "PUT":
+            return doPut(req, res, filePath);
+        case "POST":
+            return doPost(req, res, filePath);
+        case "DELETE":
+            return doDelete(req, res, filePath);
+        default:
+            return error(res, 500, "Unknown request type");
+    }
+}
+
+
+function bufferPostRequest(req, res, callback) {
+    var queryData = "";
+    req.on("data", function(data) {
+        queryData += data;
+        if (queryData.length > 1e6) {
+            queryData = "";
+            res.writeHead(413, {
+                "Content-Type": "text/plain"
+            });
+            req.connection.destroy();
+        }
+    });
+    req.on("end", function() {
+        res.post = qs.parse(queryData);
+        callback();
+    });
+}
+
+function fileList(root, res) {
+    var stop = false;
+
+    res.on("error", function() {
+        stop = true;
+    });
+    res.on("close", function() {
+        stop = true;
+    });
+    walk("", function(err) {
+        if (err) {
+            return error(res, 500, err.message);
+        }
+        res.end();
+    });
+
+    function walk(dir, callback) {
+        if (stop) {
+            return callback();
+        }
+        fs.readdir(root + dir, function(err, list) {
+            if (err) {
+                return callback(err);
+            }
+            var pending = list.length;
+            if (pending === 0) {
+                return callback(null);
+            }
+            list.forEach(function(name) {
+                if (name === "." || name === "..") {
+                    return checkDone();
+                }
+                var file = dir + '/' + name;
+                fs.stat(root + file, function(err, stat) {
+                    if (stat && stat.isDirectory()) {
+                        walk(file, function() {
+                            checkDone();
+                        });
+                    } else if (stat && stat.isFile()) {
+                        res.write(dir + '/' + name + "\n");
+                        checkDone();
+                    } else {
+                        // !stat
+                        checkDone();
+                    }
+                });
+            });
+
+            function checkDone() {
+                if (!--pending) {
+                    callback();
+                }
+            }
+        });
+    }
+}
+
+function runCommand(root, res) {
+    var command = res.post.command;
+    if (!command) {
+        return error(res, 500, "No command specified");
+    }
+    try {
+        command = JSON.parse(command);
+    } catch (e) {
+        return error(res, 500, "Could not parse command");
+    }
+    var p = spawn(command[0], command.slice(1), {
+        cwd: root,
+        env: process.env
+    });
+    res.on("error", function() {
+        console.log("Killing sub process", command[0]);
+        p.kill();
+    });
+    res.on("close", function() {
+        console.log("Killing sub process", command[0]);
+        p.kill();
+    });
+    if (res.post.stdin) {
+        p.stdin.end(res.post.stdin);
+    }
+    p.stdout.pipe(res);
+    p.stderr.pipe(res);
+    p.on("close", function() {
+        res.end();
+    });
+    p.on("error", function(err) {
+        console.error("Run error", err);
+    });
+}
